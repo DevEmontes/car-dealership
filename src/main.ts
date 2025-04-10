3e7a0851c5ef7fdfaaff35815d5fc72416ea7452
DevEmontes <erick_montes@americana.edu.co>
2025-04-10 14:13:15 -0500

    pipe globales

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // only allow properties that are defined in the DTO
+      forbidNonWhitelisted: true, // throw an error if a property is not defined in the DTO
+    }));
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
